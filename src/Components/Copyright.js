ea36a5ba4f15d7e56162e961518d0f37b510b709
nandikingsuk <84791203+nandikingsuk@users.noreply.github.com>
2022-01-09 16:35:58 +0530

    in copyright year modified

--- src/Components/Copyright.js
+++ src/Components/Copyright.js
@@ -6,7 +6,7 @@ const Copyright = () => {
             {/* copyright start here */}
             <footer className="footer mt-auto py-3 bg-dark">
             <div className="container text-center">
-                <span className="text-white">&copy; Copyright 2020-21 <Link className='text-white text-decoration-none' to="/">RISHIPAITHY</Link> | ALL RIGHTS RESERVED</span>
+                <span className="text-white">&copy; Copyright 2020-22 <Link className='text-white text-decoration-none' to="/">RISHIPAITHY</Link> | ALL RIGHTS RESERVED</span>
             </div>
             </footer>
             {/* copyright end here */}
